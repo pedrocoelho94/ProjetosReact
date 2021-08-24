ada32ce43d28459a1e645c25631b655062608531
Pedro Coelho <pedrohenriquesc@gmail.com>
2021-08-24 15:21:49 -0300

    Route & Auth

---
+++ letmeask/commands.jsx
@@ -0,0 +1,10 @@
+/*
+yarn start - inicia aplicação
+
+yarn add node-sass@^5.0.0
+
+yarn add history react-router-dom@netx
+
+//caso pacote nao tenha typescript
+yarn add @types/react-router-dom -D
+*/
