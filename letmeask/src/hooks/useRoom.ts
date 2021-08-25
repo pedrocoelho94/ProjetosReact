3dbab717a984d3e3636f5f921eec04057dec84bc
Pedro Coelho <pedrohenriquesc@gmail.com>
2021-08-25 18:34:06 -0300

    Questions Rooms

---
+++ letmeask/src/hooks/useRoom.ts
@@ -0,0 +1,82 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { database } from "../services/firebase"
+import { useAuth } from "./useAuth"
+
+type FirebaseQuestions = Record<
+   string,
+   {
+      author: {
+         name: string
+         avatar: string
+      }
+      content: string
+      isAnswered: boolean
+      isHighLighted: boolean
+      likes: Record<string, {
+         authorId: string
+      }>
+   }
+>
+
+type QuestionType = {
+   id: string
+   author: {
+      name: string
+      avatar: string
+   }
+   content: string
+   isAnswered: boolean
+   isHighLighted: boolean
+   likeCount: number
+   likeId: string | undefined
+}
+
+export function useRoom(roomId: string){
+   const {user} =  useAuth()
+   const [questions, setQuestions] = useState<QuestionType[]>([])
+   const [title, setTitle] = useState('')
+
+   useEffect(() => {
+      //consumir os dados salvos no firebase
+      const roomRef = database.ref(`rooms/${roomId}`)
+
+      /*Explicação object.entries
+      const ojb ={
+         "name": "Pedro",
+         "idade": 26
+      }
+
+      Object.entries(obj) => [ ["name", "Pedro"], ["idade": 26] ]*/
+
+      roomRef.on('value', room => {
+         const databaseRoom = room.val()
+         const firebaseQuestions: FirebaseQuestions =
+            databaseRoom.questions ?? {}
+
+         const parsedQuestions = Object.entries(firebaseQuestions).map(
+            ([key, value]) => {
+               return {
+                  id: key,
+                  content: value.content,
+                  author: value.author,
+                  isHighLighted: value.isHighLighted,
+                  isAnswered: value.isAnswered,
+                  likeCount: Object.values(value.likes ?? {}).length,
+                  likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
+               }
+            }
+         )
+
+         // console.log(parsedQuestions)
+         setTitle(databaseRoom.title)
+         setQuestions(parsedQuestions)
+      })
+
+      return () => {
+         roomRef.off('value')
+      }
+   }, [roomId, user?.id])
+
+   return {questions, title}
+}
