62b7ea8dd76fc4756bd1160f493476f1ddfc55cc
Pedro Coelho <pedrohenriquesc@gmail.com>
2021-08-28 17:06:32 -0300

    experience-bar

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,7 +1,12 @@
+import ExperienceBar from './components/ExperienceBar'
+import './styles/global.css'
+
 function App() {
-  return (
-    <h1>Hello, NLW!</h1>
-  );
+   return (
+      <div className="container">
+         <ExperienceBar />
+      </div>
+   )
 }
 
-export default App;
+export default App
